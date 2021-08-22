ef66e4bbccf0db1f788535e4612e6c8e17d249e1
ArdaGurcan <ardagurcan2004@gmail.com>
2021-08-22 17:44:16 +0300

    swap mixed up files names

--- src/shaders/fragment/color_2d.rs
+++ src/shaders/fragment/color_2d.rs
@@ -4,9 +4,7 @@ pub const SHADER: &str = r#"
     uniform vec4 uColor;
     uniform float uOpacity;
 
-    varying lowp vec4 vColor;
-
     void main() {
-        gl_FragColor = vec4(vColor.r, vColor.g, vColor.b, vColor.a * uOpacity);
+        gl_FragColor = vec4(uColor.r, uColor.g, uColor.b, uColor.a * uOpacity);
     }
 "#;
